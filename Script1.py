c1790924d5c2d15739392a84eaed86882393b6ce
AUF-777 <chukchee13@gmail.com>
2026-04-12 18:33:40 +0300

    Update Script1.py

--- Script1.py
+++ Script1.py
@@ -19,18 +19,20 @@ LEVL_WIDTH = 2200
 bg_img = pygame.image.load('assets/image/bg.png')
 bg_img = pygame.transform.scale(bg_img, (WIDTH, HEIGHT))
 
-player_img = pygame.image.load('assets/image/player.png')
+player_img = pygame.image.load('assets/image/player (2).png')
 player_img = pygame.transform.scale(player_img, (40,50))
+player_img_zerckal = pygame.image.load('assets/image/zerckal.png')
+player_img_zerckal = pygame.transform.scale(player_img_zerckal, (40,50))
 
 enemy_img = pygame.image.load('assets/image/enemy.png')
 enemy_img = pygame.transform.scale(enemy_img, (40,40))
 
 
-platform_img = pygame.image.load('assets/image/platform.png')
-platform_img = pygame.transform.scale(platform_img, (200,40))
-
 portal_img = pygame.image.load('assets/image/portal.png')
 portal_img = pygame.transform.scale(portal_img, (40,60))
+
+okonchan_img = pygame.image.load('assets/image/fon_okonchan.jpg')
+okonchan_img = pygame.transform.scale(okonchan_img, (WIDTH,HEIGHT))
 
 coin = [
     pygame.transform.scale(pygame.image.load('assets/image/coin1.png'), (20,20)),
@@ -41,9 +43,16 @@ coin = [
 
 class Platform:
     def __init__(self,x,y,w,h):
+        self.flag = 1
         self.rect = pygame.Rect(x, y, w, h)
+        self.platform_img = pygame.image.load('assets/image/platform.png')
+        self.platform_img = pygame.transform.scale(self.platform_img, (self.rect.width, self.rect.height))
+        self.platform_img2 = pygame.image.load('assets/image/platform_lefitat.png')
+        self.platform_img2 = pygame.transform.scale(self.platform_img2, (self.rect.width, self.rect.height))
     def draw(self, surf, camera_x = 0):
-        pygame.draw.rect(surf, (60,180,90), (self.rect.x - camera_x, self.rect.y, self.rect.w, self.rect.h))
+        screen.blit(self.platform_img, (self.rect.x - camera_x, self.rect.y, self.rect.w, self.rect.h))
+    def draw2(self, surf, camera_x = 0):
+        screen.blit(self.platform_img2, (self.rect.x - camera_x, self.rect.y, self.rect.w, self.rect.h))
 class Coin:
     def __init__(self,x,y):
         self.rect = pygame.Rect(x, y, 20, 20)
@@ -86,6 +95,7 @@ class Player:
 
         self.lives = 3
         self.invuln = 0
+        self.umage = player_img
 
     def jump(self):
 
@@ -105,8 +115,10 @@ class Player:
 
         dx = 0
         if key[pygame.K_a]:
+            self.umage = player_img_zerckal
             dx -= self.speed
         if key[pygame.K_d]:
+            self.umage = player_img
             dx += self.speed
 
         self.rect.x += dx
@@ -141,15 +153,22 @@ class Player:
 
         if self.invuln > 0 and (self.invuln % 10) < 5:
             return
-        screen.blit(player_img, (self.rect.x - camera_x, self.rect.y, self.rect.w, self.rect.h))
+        screen.blit(self.umage, (self.rect.x - camera_x, self.rect.y, self.rect.w, self.rect.h))
 
 class Game:
     def __init__(self):
         self.reset()
     def reset(self):
         self.player = Player()
+
+        self.Platforms2 = []
+
+        for x in range(0, LEVL_WIDTH, 300):
+            self.Platforms2.append(Platform(x, HEIGHT - 40, 400, 40))
+
+        self.Platforms2 += []
+
         self.Platforms = [
-            Platform(0,HEIGHT - 40, LEVL_WIDTH, 40),
             Platform(140, 330, 180, 20),
             Platform(380,260,180,20),
             Platform(610,320,140,20),
@@ -158,6 +177,7 @@ class Game:
             Platform(1500, 340, 220, 20),
             Platform(1800, 280, 220, 20)
         ]
+
 
         self.coins = [
             Coin(200,300),
@@ -230,10 +250,15 @@ class Game:
                 self.check_finish()
                 self.update_camera()
 
-            screen.blit(bg_img, (0,0))
-            screen.blit(portal_img, (self.finish.x - self.camera_x, self.finish.y))
-            for Platform in self.Platforms:
-                Platform.draw(screen, self.camera_x)
+                screen.blit(bg_img, (0,0))
+                screen.blit(portal_img, (self.finish.x - self.camera_x, self.finish.y))
+
+
+
+            for a in self.Platforms:
+                a.draw2(screen, self.camera_x)
+            for p in self.Platforms2:
+                p.draw(screen, self.camera_x)
 
             for с in self.coins:
                 с.draw(screen, self.camera_x)
@@ -247,13 +272,13 @@ class Game:
 
 
             if self.game_over:
+                screen.blit(okonchan_img, (0, 0))
 
-                t1 = big_font.render("Ты проиграл", True, (200, 0, 0))
-                t2 = font.render("Нажми R для повторной игры", True, (200, 0, 0))
+                t1 = big_font.render("Ты проиграл", True, (255, 255, 255))
+                t2 = font.render("Нажми R для повторной игры", True, (255, 255, 255))
 
                 screen.blit(t1, t1.get_rect(center=(WIDTH/2, HEIGHT/2 - 20)))
                 screen.blit(t2, t2.get_rect(center=(WIDTH / 2, HEIGHT / 2 + 25)))
-
             pygame.display.flip()
             clock.tick(60)
 
